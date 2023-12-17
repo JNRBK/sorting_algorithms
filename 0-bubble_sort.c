8abfed48a0b6fe07879be3d0b4f53fd751110254
JNRBK <rafeekmorgan@yahoo.com>
2023-12-17 11:02:03 +0200

    as

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,16 +1,18 @@
 #include "sort.h"
 void bubble_sort(int *array, size_t size)
 {
-	size_t j, temp = 0;
+	size_t j, i;
+	size_t temp = 0;
 
 	for (j = 0; j < size - 1; j++)
 	{
-		if (array[j] > array[j + 1])
-		{
-			temp = array[j];
-			array[j] = array[j + 1];
-			array[j + 1] = temp;
-			print_array(array, size);
-		}
+		for (i = 0; i < size - j - 1; i++)
+			if (array[j] > array[j + 1])
+			{
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+				print_array(array, size);
+			}
 	}
 }
