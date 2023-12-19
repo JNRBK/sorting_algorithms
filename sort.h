9fd8ac6891ba0757d0df9ba79d219413b73ef8f3
JNRBK <rafeekmorgan@yahoo.com>
2023-12-19 17:02:57 +0200

    as

--- sort.h
+++ sort.h
@@ -24,7 +24,7 @@ void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
-void swap(int* x, int* y);
+void swap(int *x, int *y);
 
 
 #endif
