d1bbf0588168832bd98b44c0e0e06591a8bc3164
JNRBK <rafeekmorgan@yahoo.com>
2023-12-19 16:49:40 +0200

    a

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,4 +1,17 @@
 #include "sort.h"
+
+/**
+ * swap - function that swap two elements
+ * @x: element
+ * @y: element
+ * Return: nothing
+*/
+void swap(int *x, int *y)
+{
+	int temp = *x;
+	*x = *y;
+	*y = temp;
+}
 /**
  * bubble_sort - function using bubble sort method
  * @array: array holding values to be sorted
@@ -8,17 +21,21 @@
 void bubble_sort(int *array, size_t size)
 {
 	size_t j, i;
-	size_t temp;
+	int swp;
 
 	for (j = 0; j < size - 1; j++)
 	{
+		swp = 0;
 		for (i = 0; i < size - j - 1; i++)
 			if (array[i] > array[i + 1])
 			{
-				temp = array[i];
-				array[i] = array[i + 1];
-				array[i + 1] = temp;
+				swap(&array[i], &array[i + 1]);
 				print_array(array, size);
+				swp = 1;
 			}
+	}
+	if (!swp)
+	{
+		return;
 	}
 }
